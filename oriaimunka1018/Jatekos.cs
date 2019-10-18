3f328fe542a11cc0acd4e558592e51b75210fad4
Turan.Balazs@VEREB.DC <Turan.Balazs@VEREB.DC>
2019-10-18 11:35:39 +0200

    Add project files.

---
+++ oriaimunka1018/Jatekos.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oriaimunka1018
+{
+    class Jatekos
+    {
+        public string Nev { get; set; }
+        public int EvesFizu { get; set; }
+        public int Evek { get; set; }
+
+        public Jatekos(string nev, int evesFizu, int evek)
+        {
+            Nev = nev;
+            EvesFizu = evesFizu;
+            Evek = evek;
+        }
+    }
+}
